d2f59762a915cf34b115912f055e66d8d26cc8a2
alazovski <alexander.lazovsky@megafon.ru>
2022-09-16 02:51:49 +0300

    add messageConfig

--- README.md
+++ README.md
@@ -24,13 +24,13 @@ allprojects {
 <dependency>
 	 <groupId>com.github.Lazovski1991</groupId>
 	 <artifactId>forAllCore</artifactId>
-	 <version>0.1.0</version>
+	 <version>0.2.0</version>
 </dependency>
 ````
 
 Для gradle:
 ````
 dependencies {
-   implementation 'com.github.Lazovski1991:forAllCore:0.1.0'
+   implementation 'com.github.Lazovski1991:forAllCore:0.2.0'
 }
 ````
---
+++ src/main/java/my/company/core/configuration/MessageConfig.java
@@ -0,0 +1,37 @@
+package my.company.core.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class MessageConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor
+                = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("language");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource =
+                new ResourceBundleMessageSource();
+        messageSource.setBasenames("language/messages");
+        messageSource.setDefaultLocale(new Locale("ru"));
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
