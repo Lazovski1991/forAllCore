566a4258c4f1f0d6de6c0dc6d9da05474952afc2
alazovski <alexander.lazovsky@megafon.ru>
2022-09-17 03:06:04 +0300

    add general config

---
+++ src/main/java/my/company/core/configuration/GeneralConfig.java
@@ -0,0 +1,37 @@
+package my.company.core.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.TimeZone;
+
+@Configuration
+public class GeneralConfig {
+    @Value("${cors.allowedOrigins:}#{T(java.util.Collections).emptyList()}")
+    private List<String> allowedOrigins;
+
+    @Bean
+    @ConditionalOnProperty(prefix = "cors", name = "enabled", havingValue = "false", matchIfMissing = false)
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                allowedOrigins
+                        .forEach(alOr -> registry.addMapping("/**").allowedOrigins(alOr));
+            }
+        };
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        System.out.println("Date in UTC: " + LocalDateTime.now());
+    }
+}
